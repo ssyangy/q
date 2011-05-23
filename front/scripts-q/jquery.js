1f221025ccf16a7eb3a4c817a8e4e83cd7f90e54
zicjin <zicjin@gmail.com>
2011-05-23 11:54:45 +0800

    merge

--- front/scripts-q/jquery.js
+++ front/scripts-q/jquery.js
@@ -1,5 +1,5 @@
 
-define('jquery-debug', [], function(require,exports,module) {
+define('jquery', [], function(require,exports,module) {
 /*!
  * jQuery JavaScript Library v1.6.1
  * http://jquery.com/
